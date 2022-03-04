411e9252f3356401850a49225ef0b65c4ba92635
woopaca <jcw001031@gmail.com>
2022-03-04 21:18:27 +0900

    modify test.c

--- test/test/main.c
+++ test/test/main.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
-int sumEven(int a){
+int even=0;
+int odd=0;
+
+void sumEven(int a){
+    even+=a;
+}
+
+void sumOdd(int a){
+    odd+=a;
+}
+
+void sumPrint(int a, int b){
+    printf("Even number = %d, Odd number = %d", a, b);
+}
+
+int main(int argc, char* argv[]) {
+    int i;
+    for(i=1;i<101;i++){
+        if(i%2==0)
+            sumEven(i);
+        else
+            sumOdd(i);
+    }
     
-}
-
-int sumOdd(int a){
-    return 0;
-}
-
-void sum(){
+    sumPrint(even, odd);
     
-}
-
-int main() {
+    printf("\n\n\n");
+    
+    char s[5][30];
+    
+    for (i=0;i<5;i++){
+        printf("name : ");
+        gets(s[i]);
+    }
+    for (i=0;i<5;i++){
+        puts(s[i]);
+    }
     
     return 0;
 }
