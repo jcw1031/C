32a19dd2ffd3049420bfa962143bc9eccf6601f0
woopaca <jcw001031@gmail.com>
2022-03-24 20:39:04 +0900

    test

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -3,13 +3,31 @@
 
 using namespace std;
 int main(){
-    cout<<"Hello world"<<endl;
-    int a;
-    cout<<"what : ";
-    cin>>a;
-    cout<<a<<endl;
-    cout<<"name : ";
-    char name[30];
-    cin>>name;
-    cout<<name;
-}
+    cout<<"영문 텍스트를 입력하세요. 히스토그램을 그립니다.\n텍트의 끝은 ; 입니다. 10000개까지 가능합니다."<<endl;
+    char a[10000];
+    cin.getline(a, 10000, ';');
+    int len=strlen(a);
+    int number=0;
+    for(int i=0;i<len;i++){
+        a[i]=towlower(a[i]);
+        if(isalpha(a[i])==1){
+            number++;
+        }
+    }
+    cout<<"알파벳 수 : "<<number<<endl;
+
+    for(int i=97;i<123;i++){
+        int num=0;
+        cout<<(char)i<<" ";
+        for(int j=0;j<len;j++){
+            if((char)i==a[j]){
+                num++;
+            }
+        }
+        cout<<"("<<num<<")"<<"  :    ";
+        for(int k=0;k<num;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+} 
